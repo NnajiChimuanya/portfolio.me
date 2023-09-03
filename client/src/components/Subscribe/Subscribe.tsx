b1bdd1838d5fead975ef4587c55a42ad6decf91a
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2023-09-03 18:46:51 +0100

    fixed the subscribeModal overlay bug

--- client/src/components/Subscribe/Subscribe.tsx
+++ client/src/components/Subscribe/Subscribe.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { PortfolioContext } from "../../context/Context";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Subscribe = () => {
   const { state, dispatch } = useContext(PortfolioContext);
@@ -9,8 +10,26 @@ const Subscribe = () => {
   };
 
   return (
-    <div onClick={handleShowSubscribeModal} className="subscribeModalContainer">
-      <div className="subscribeModal">Subscribe</div>
+    <div className="subscribeModalContainer">
+      <div
+        className="subscribeModalOverlay"
+        onClick={handleShowSubscribeModal}
+      ></div>
+      <div className="subscribeModal">
+        <div className="close">
+          <AiOutlineClose
+            className="closeIcon"
+            onClick={handleShowSubscribeModal}
+          />
+        </div>
+
+        <div className="section1">
+          <img src="me.jpg" className="subscribeImage" alt="Muanya" />
+          <h4>Subscribe to Nnaji Chimuanya</h4>
+          <p>Stay up to date with everything important</p>
+          <input type={"text"} placeholder="Email" />
+        </div>
+      </div>
     </div>
   );
 };
